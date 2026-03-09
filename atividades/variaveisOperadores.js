03d0bdc001f64fb1c2c0028b06b264d90f779bc0
IsaquielNathan <isaquielnthan@gmail.com>
2026-03-09 17:29:07 -0300

    Adicionando atividade 1 com todas as operações

---
+++ atividades/variaveisOperadores.js
@@ -0,0 +1,11 @@
+let A = 10;
+let B = 5;
+let soma = A + B;
+let subtracao =  A - B;
+let divisao = A / B;
+let multiplicacao = A * B;
+
+console.log(`A soma entre ${A} e ${B} é igual a ${soma}`);
+console.log(`A subtração entre ${A} e ${B} é igual a ${subtracao }`);.
+console.log(`A divisão entre ${A} e ${B} é igual a ${divisao }`);
+console.log(`A multiplicação entre ${A} e ${B} é igual a ${multiplicacao}`); 
