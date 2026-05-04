e60471cf35debed7d1e09081927b215065453cf4
IsaquielNathan <isaquielnthan@gmail.com>
2026-05-04 20:36:43 -0300

    atualizações no código

--- atividades/atividade 5/index.js
+++ atividades/atividade 5/index.js
@@ -9,18 +9,31 @@ const app = express();
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+    let dados_agendamento
     res.render("index.html");
 });
 
-app.post('/agendamento', (req, res)=> {
+app.post('/agendamento', (req, res) => {
     dados_agendamento = req.body;
     console.log(dados_agendamento);
-    res.render('agendamento.html', {dados_agendamento});
 });
+
+if (
+    dados_agendamento.nome == null ||
+    dados_agendamento.nome.length == 0 ||
+    dados_agendamento.nome.trim() == "" 
+){
+    res.redirect ('/?erro_nome=true');
+}
+
+else {
+    res.render('agendamento.html', { dados_agendamento });
+};
 
 app.listen(PORT, () => {
     console.log('App rodando na porta ' + PORT);
 });
+
