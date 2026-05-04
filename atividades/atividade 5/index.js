705dc647baa67ad44b588c0e120351f57d9d8759
IsaquielNathan <isaquielnthan@gmail.com>
2026-05-04 15:44:26 -0300

    Adicionando mudanças no código colocando validações faltosas

--- atividades/atividade 5/index.js
+++ atividades/atividade 5/index.js
@@ -3,6 +3,9 @@ const mustacheExpress = require('mustache-express');
 const PORT = 8080;
 const app = express();
 
+// npm install mustache-express
+// Exemplo : http://localhost:8080
+
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
@@ -12,10 +15,10 @@ app.get("/", (req, res) => {
     res.render("index.html");
 });
 
-app.post('/agendamento.', (req, res)=> {
+app.post('/agendamento', (req, res)=> {
     dados_agendamento = req.body;
     console.log(dados_agendamento);
-    res.send(dados_agendamento);
+    res.render('agendamento.html', {dados_agendamento});
 });
 
 app.listen(PORT, () => {
